8c5c85ad5ada2da793f955c89526fcda16274d03
amelialilyosborne <amelia_osborne@biswashington.org>
2025-03-13 13:49:48 +0000

    amelia

--- amelia.py
+++ amelia.py
@@ -1,1 +1,14 @@
 
+average = int(input("What is your score?:"))
+if average >= 100:
+    print ("A star")
+elif average >= 90:
+    print("A")
+elif average >= 80:
+    print("B")
+elif average >= 60:
+    print("C")
+elif average >= 40:
+    print("D")
+else:
+    print("F")
